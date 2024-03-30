7d48bc269fc5359f7d4dff79b059278562a03ae1
Anirudh Chidambaram <anirudh.chida@gmail.com>
2024-03-31 01:53:34 +1100

    Made a basic server side functionality and a new webpage with buttons

--- web_flask.py
+++ web_flask.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder="templates")
 
@@ -6,6 +6,23 @@ app = Flask(__name__, template_folder="templates")
 @app.route("/")
 def hello():
     return render_template("home.html")
+
+
+@app.route("/add_numbers")
+def add_numbers():
+    return render_template("add_numbers.html")
+
+
+@app.route("/calculate", methods=["POST"])
+def calculate_sum():
+    print(request.data)
+    print(request.args.get("num1"))
+    num_1 = int(request.form["num1"])
+    num_2 = int(request.form["num2"])
+    print(num_1, num_2)
+    ans = num_1 + num_2
+    # print(ans)
+    return render_template("add_numbers.html", result=ans)
 
 
 if __name__ == "__main__":
